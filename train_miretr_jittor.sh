b997898800956212812fd8bd2f5d953002916c82
ZhiyuanYu317 <45563073+zhiyuanYU134@users.noreply.github.com>
2024-09-26 00:09:43 +0800

    Update train_miretr_jittor.sh

--- train_miretr_jittor.sh
+++ train_miretr_jittor.sh
@@ -3,7 +3,7 @@
 for((i=0;i<60;i++))
 do
 echo $i;
-/home/yuzhiyuan/.conda/envs/miretr/bin/python /home/yuzhiyuan/GeoTransformer-main/experiments/scan2cad_jittor/trainval.py --epochs $i;
+python experiments/scan2cad_jittor/trainval.py --epochs $i;
 done
 
 
